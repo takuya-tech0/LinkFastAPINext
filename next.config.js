25c0b9d6c272394984bb433d827d36b5a4dd551a
taku <let.smakeawish1230@gmail.com>
2025-01-05 10:16:34 +0900

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        // Reference a variable that was defined in the .env file and make it available at Build Time
-        API_ENDPOINT: process.env.API_ENDPOINT,
-      },
+  output: 'standalone',
+  env: {
+    // Reference a variable that was defined in the .env file and make it available at Build Time
+    API_ENDPOINT: process.env.API_ENDPOINT,
+  },
 }
 
 module.exports = nextConfig
